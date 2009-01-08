f568d315409658e0ad231d00b5d8f940f2b28d0c
Andrew McNabb <amcnabb@mcnabbs.org>
2009-01-08 14:35:01 -0700

    imported pssh 1.3.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import os
 setup(
     name = "pssh",
-    version = "1.3.1",
+    version = "1.3.2",
     author = "Brent N. Chun",
     url = "http://www.theether.org/pssh/",
     license = "BSD",
