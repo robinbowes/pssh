9d177f8926ed83f75fd386e0091b1ab45114448e
Andrew McNabb <amcnabb@mcnabbs.org>
2009-10-20 15:13:20 -0600

    updated the COPYING and AUTHORS files

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ import os
 setup(
     name = "pssh",
     version = "1.4.3",
-    author = "Brent N. Chun",
-    url = "http://www.theether.org/pssh/",
+    author = "Andrew McNabb",
+    url = "http://code.google.com/p/parallel-ssh/",
     license = "BSD",
     packages = find_packages(),
     scripts = [os.path.join("bin", p) for p in ["pssh", "pnuke", "prsync", "pslurp", "pscp"]]
