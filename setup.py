10d5f03355d3c640adebcdf5b5adb31071e670fe
Andrew McNabb <amcnabb@mcnabbs.org>
2009-10-20 16:18:59 -0600

    bumped the version to 2.0 and updated the Changelog

--- setup.py
+++ setup.py
@@ -1,11 +1,31 @@
 from setuptools import setup, find_packages
 import os
+
+long_description = """PSSH (Parallel SSH) provides parallel versions of OpenSSH and related tools, including pssh, pscp, prsync, pnuke, and pslurp.  The project includes psshlib which can be used within custom applications."""
+
 setup(
     name = "pssh",
-    version = "1.4.3",
+    version = "2.0",
     author = "Andrew McNabb",
+    author_email = "amcnabb@mcnabbs.org",
     url = "http://code.google.com/p/parallel-ssh/",
+    description = "Parallel version of OpenSSH and related tools",
+    long_description = long_description,
     license = "BSD",
+    platforms = ['linux'],
+
+    classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: System :: Clustering",
+        "Topic :: System :: Networking",
+        "Topic :: System :: Systems Administration",
+        ],
+
     packages = find_packages(),
     scripts = [os.path.join("bin", p) for p in ["pssh", "pnuke", "prsync", "pslurp", "pscp"]]
     )
